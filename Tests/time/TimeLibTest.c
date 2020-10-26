9041c1fe2373d8e5dcb7362aa21c8aeb785d6322
João Victor Cavalcanti <56661781+John-Cavalcanti@users.noreply.github.com>
2020-10-26 15:32:39 -0300

    Add files via upload

--- Tests/time/TimeLibTest.c
+++ Tests/time/TimeLibTest.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+#include <conio.h>
+
 void desenha_cara(int x)
 {
-    if (x = 1)
+    if (x == 1)
     {
         printf(" O \n");
         printf(")|\\\n");
         printf("/ )");
     }
-    else if (x = 2)
+    else if (x == 2)
     {
         printf(" O \n");
         printf("/|)\n");
@@ -22,7 +23,7 @@ void desenha_cara(int x)
         printf("/|\\\n");
         printf("/ \\ ");
     }
-} */
+}
 
 /* int main(void)
 {
@@ -43,13 +44,46 @@ void desenha_cara(int x)
     return 0;
 }
  */
+
+
 void delay(ms)
 {
     clock_t timeDelay = ms + clock();
-    while(timeDelay > clock());
+    while (timeDelay > clock());
 }
 
 int main(void)
 {
+    int x = 0, moment;
+    time_t start, end;
+    long unsigned t;
 
+    start = time(NULL);
+
+    for (x = 0; x < 500; x++)
+    {
+        end = time(NULL);
+        moment = difftime(end, start);
+
+        //printf("%.1f",moment);
+
+        if (moment % 2 == 0)
+        {
+            desenha_cara(1);
+        }
+        else if (moment % 2 != 0)
+        {
+            desenha_cara(2);
+        }
+        else
+        {
+            desenha_cara(0);
+        }
+
+        system("cls");
+    }
+    if (getch() == 83)
+    {
+        return 0;
+    }
 }
