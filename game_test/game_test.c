85e6d55cc835ba8aaf2029f8a2cdf0f909f8f369
Gabriel Pereira <56116468+gabrielScript-dev@users.noreply.github.com>
2020-11-11 01:43:10 -0300

    Update game_test.c

--- game_test/game_test.c
+++ game_test/game_test.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <conio.h>
 #include <windows.h>
-#include <time.h>
 
-// Boa parte das variáveis desse código estão no escopo (global) do código para facilitar na manutenção deste.
-int dino_x = 20;
-int cactus_x = 80;
-int cactus_y = 15;
-int dino_y = 15;
+const int time_dino = 50;
+const int time_cactus = 50;
 
-//velocidade da movimentação do cacto 1 - EASY 2 - MEDIUM 3 - HARD
-int velocity = 1;
-
-unsigned int score = 0;
+unsigned int score;
 unsigned long score_time = 0;
+unsigned long animation_time = 0;
 
-const int time_dino = 101;
-const int time_cactus = 100;
+int a = 90;
+int b = 90;
 
+char cactus_xB[] =  {0, 0, 0, 0, 0, 0, 0};
+char cactus_xA[] =  {0, 0, 0, 0, 0, 0, 0};
+char cactus_y[] =  {26, 27, 28, 29, 30, 31, 32};
+
+char dino_x[] = {22, 21, 21, 21, 21, 14, 13, 13, 13, 16, 16, 16, 17, 17, 18};
+char dino_y[] = {18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
 
 void gotoxy(int x, int y)
 {
@@ -27,128 +29,452 @@ void gotoxy(int x, int y)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-void delay(unsigned int mseconds)
+void delay(int ms)
 {
-    clock_t goal = mseconds + clock();
-    while (goal > clock());
+    clock_t timeDelay = ms + clock();
+    while (timeDelay > clock());
 }
 
-/* 
- * Essa função realiza a computação da pontuação a cada 1 segundo.
- * Nota: A condição presente no if funciona semelhante a função delay
- */
-void time_score() {
+void draw_time_score()
+{
 
-    if(clock() - score_time >= 1000) 
+    if(clock() - score_time >= 100)
     {
         score_time = clock();
-        gotoxy(45, 0);
+        gotoxy(80, 4);
         printf("SCORE: %d", score);
         score += 1;
     }
 }
 
-/*
- * Essa função configura a coloração do terminal e defini um layout. 
- * Além disso, imprime informações referentes aos componentes do jogo
- * (Coordenadas do dinossauro, o próprio e o score). 
- */
-void setup()
+void set_cactus(int x, int cactus)
 {
-    system("mode con: lines=29 cols=82");
-    system("color a");
-    gotoxy(0, 0);
-    printf("X: %d | Y: %d", dino_x, dino_y);
-    gotoxy(45, 0);
-    printf("SCORE: 0");
-    gotoxy(dino_x, dino_y);
-    printf("A");
+    switch(cactus)
+    {
+        case 1:
+            for (int i = 0; i < 7; ++i)
+            {
+                cactus_xA[i] = x;
+            }
+            break;
+
+        case 2:
+            for (int i = 0; i < 7; ++i)
+            {
+                cactus_xB[i] = x;
+            }
+            break;
+    }
 }
 
-/*
- * Essa função realiza a ação de fazer com que o cacto possa mover-se
- * no jogo, através da adição de valores na coordena x do cacto. Além disso, 
- * verifica se o cacto chegou até chegou na coordena x = 0 e Caso aconteça irá 
- * imprimilo novamente na coordena x = 80;
- */
+void draw_cactusA()
+{
+    int x = -1;
+
+    if(cactus_xA[3] == 10)
+    {
+        gotoxy(cactus_xA[0], cactus_y[0]);
+        printf("      ");
+        gotoxy(cactus_xA[1], cactus_y[1]);
+        printf("         ");
+        gotoxy(cactus_xA[2], cactus_y[2]);
+        printf("         ");
+        gotoxy(cactus_xA[3], cactus_y[3]);
+        printf("         ");
+        gotoxy(cactus_xA[4], cactus_y[4]);
+        printf("         ");
+        gotoxy(cactus_xA[5], cactus_y[5]);
+        printf("         ");
+        gotoxy(cactus_xA[6], cactus_y[6]);
+        printf("         ");
+
+        set_cactus(a, 1);
+
+    }
+    else
+    {
+        int i;
+        for (i = 0; i < 7; ++i)
+        {
+            cactus_xA[i] += x;
+        }
+
+        gotoxy(cactus_xA[0], cactus_y[0]);
+        printf("    a_ ");
+        gotoxy(cactus_xA[1], cactus_y[1]);
+        printf(" _ ( )   ");
+        gotoxy(cactus_xA[2], cactus_y[2]);
+        printf("( \\| | _");
+        gotoxy(cactus_xA[2] + 8, cactus_y[2]);
+        printf(" ");
+        gotoxy(cactus_xA[3], cactus_y[3]);
+        printf(" \\,. |/ )");
+        gotoxy(cactus_xA[3] + 9, cactus_y[3]);
+        printf(" ");
+        gotoxy(cactus_xA[4], cactus_y[4]);
+        printf("   |  /'");
+        gotoxy(cactus_xA[4] + 8, cactus_y[4]);
+        printf(" ");
+        gotoxy(cactus_xA[5], cactus_y[5]);
+        printf("   | |");
+        gotoxy(cactus_xA[5] + 6, cactus_y[5]);
+        printf(" ");
+        gotoxy(cactus_xA[6], cactus_y[6]);
+        printf("   | |");
+        gotoxy(cactus_xA[6] + 6, cactus_y[6]);
+        printf(" ");
+    }
+}
+
+void draw_cactusB()
+{
+    int x = -1;
+
+    if(cactus_xB[3] == 5)
+    {
+        gotoxy(cactus_xB[0], cactus_y[0]);
+        printf("      ");
+        gotoxy(cactus_xB[1], cactus_y[1]);
+        printf("         ");
+        gotoxy(cactus_xB[2], cactus_y[2]);
+        printf("         ");
+        gotoxy(cactus_xB[3], cactus_y[3]);
+        printf("         ");
+        gotoxy(cactus_xB[4], cactus_y[4]);
+        printf("         ");
+        gotoxy(cactus_xB[5], cactus_y[5]);
+        printf("         ");
+        gotoxy(cactus_xB[6], cactus_y[6]);
+        printf("         ");
+
+        if(score >= 10 && cactus_xA[3] <= 40)
+        {
+            if(cactus_xA[3] >= 40){
+                set_cactus(b, 2);
+            }
+        }
+    }
+    else
+    {
+        int i;
+        for (i = 0; i < 7; ++i)
+        {
+            cactus_xB[i] += x;
+        }
+
+        gotoxy(cactus_xB[0], cactus_y[0]);
+        printf("    b_ ");
+        gotoxy(cactus_xB[1], cactus_y[1]);
+        printf(" _ ( )   ");
+        gotoxy(cactus_xB[2], cactus_y[2]);
+        printf("( \\| | _");
+        gotoxy(cactus_xB[2] + 8, cactus_y[2]);
+        printf(" ");
+        gotoxy(cactus_xB[3], cactus_y[3]);
+        printf(" \\,. |/ )");
+        gotoxy(cactus_xB[3] + 9, cactus_y[3]);
+        printf(" ");
+        gotoxy(cactus_xB[4], cactus_y[4]);
+        printf("   |  /'");
+        gotoxy(cactus_xB[4] + 8, cactus_y[4]);
+        printf(" ");
+        gotoxy(cactus_xB[5], cactus_y[5]);
+        printf("   | |");
+        gotoxy(cactus_xB[5] + 6, cactus_y[5]);
+        printf(" ");
+        gotoxy(cactus_xB[6], cactus_y[6]);
+        printf("   | |");
+        gotoxy(cactus_xB[6] + 6, cactus_y[6]);
+        printf(" ");
+    }
+}
 
 void cactus(int time)
 {
-    gotoxy(cactus_x, cactus_y);
-    printf("*");
-    gotoxy(cactus_x+velocity, cactus_y);
-    printf(" ");
 
-    if(cactus_x == 0)
+    if(score >= 100)
     {
-        cactus_x = 80;
-        gotoxy(0, cactus_y);
-        printf(" ");
+        draw_cactusA();
+        draw_cactusB();
+    }
+    else
+    {
+        draw_cactusA();
     }
 
-    if(score >= 15 && cactus_x == 80)
-        velocity = 2;
-    if(score >= 25 && cactus_x == 80)
-        velocity = 4;
-
-    cactus_x -= velocity;
     delay(time);
+}
 
-    gotoxy(70, 0);
-    printf("C: %d", cactus_x); //x axis position cactus 
+void draw_dino()
+{
+    gotoxy(dino_x[0], dino_y[0]);
+    printf("%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[1], dino_y[1]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 254, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[2], dino_y[2]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[3], dino_y[3]);
+    printf("%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[4], dino_y[4]);
+    printf("%c%c%c%c%c%c%c%c%c",  221, 221, 221, 221, 221, 221, 221, 221,221);
+    gotoxy(dino_x[5], dino_y[5]);
+    printf("%c     %c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[6], dino_y[6]);
+    printf("%c%c    %c%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[7], dino_y[7]);
+    printf("%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[8], dino_y[8]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[9], dino_y[9]);
+    printf("%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[10], dino_y[10]);
+    printf("%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    //legs
+    gotoxy(dino_x[11], dino_y[11]);
+    printf("%c%c%c  %c%c%c", 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[12], dino_y[12]);
+    printf("%c%c   %c%c", 221, 221, 221, 221);
+    gotoxy(dino_x[13], dino_y[13]);
+    printf("%c%c    %c", 221, 221, 221);
+    gotoxy(dino_x[14], dino_y[14]);
+    printf("%c%c   %c%c", 221, 221, 221, 221);
 
 }
 
-/*
- * Essa função realiza a ação de fazer com que o dinossauro possa pular
- * no jogo, através de laços de repetições que irão adiciona valores na 
- * coordenada y do dinossauro. Além disso, realiza a verificação para saber
- * se o dinossauro encontra-se no chão (on_ground) para que tal ação seja 
- * válida. Caso o contrário irá verificar se existe colisão. 
- * (Coordenadas do dinossauro, o próprio e o score). 
- */
+void draw_dino_up(int y)
+{
+    int i;
+    for(i = 0; i <= 15; i++)
+        dino_y[i] -= y;
+
+    gotoxy(dino_x[0], dino_y[0]);
+    printf("%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[0], dino_y[0] + y);
+    printf("                ");
+    gotoxy(dino_x[1], dino_y[1]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 254, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[1], dino_y[1] + y);
+    printf("                      ");
+    gotoxy(dino_x[2], dino_y[2]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[2], dino_y[2] + y);
+    printf("                      ");
+    gotoxy(dino_x[3], dino_y[3]);
+    printf("%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[3], dino_y[3] + y);
+    printf("            ");
+    gotoxy(dino_x[4], dino_y[4]);
+    printf("%c%c%c%c%c%c%c%c%c",  221, 221, 221, 221, 221, 221, 221, 221,221);
+    gotoxy(dino_x[4], dino_y[4] + y);
+    printf("                  ");
+    gotoxy(dino_x[5], dino_y[5]);
+    printf("%c     %c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[5], dino_y[5] + y);
+    printf("                     ");
+    gotoxy(dino_x[6], dino_y[6]);
+    printf("%c%c    %c%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[6], dino_y[6] + y);
+    printf("                                ");
+    gotoxy(dino_x[7], dino_y[7]);
+    printf("%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[7], dino_y[7] + y);
+    printf("                                ");
+    gotoxy(dino_x[8], dino_y[8]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[8], dino_y[8] + y);
+    printf("                          ");
+    gotoxy(dino_x[9], dino_y[9]);
+    printf("%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[9], dino_y[9] + y);
+    printf("                    ");
+    gotoxy(dino_x[10], dino_y[10]);
+    printf("%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[10], dino_y[10] + y);
+    printf("                  ");
+
+    //legs
+    gotoxy(dino_x[11], dino_y[11]);
+    printf("%c%c%c  %c%c%c", 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[11], dino_y[11] + y);
+    printf("              ");
+    gotoxy(dino_x[12], dino_y[12]);
+    printf("%c%c   %c%c", 221, 221, 221, 221);
+    gotoxy(dino_x[12], dino_y[12] + y);
+    printf("           ");
+    gotoxy(dino_x[13], dino_y[13]);
+    printf("%c%c    %c", 221, 221, 221);
+    gotoxy(dino_x[13], dino_y[13] + y);
+    printf("          ");
+    gotoxy(dino_x[14], dino_y[14]);
+    printf("%c%c   %c%c", 221, 221, 221, 221);
+    gotoxy(dino_x[14], dino_y[14] + y);
+    printf("           ");
+}
+
+void draw_dino_down(int y)
+{
+    int i;
+    for(i = 0; i <= 15; i++)
+        dino_y[i] += y;
+
+
+    gotoxy(dino_x[0], dino_y[0] - y);
+    printf("                ");
+    gotoxy(dino_x[1], dino_y[1] - y);
+    printf("                      ");
+    gotoxy(dino_x[2], dino_y[2] - y);
+    printf("                      ");
+    gotoxy(dino_x[3], dino_y[3] - y);
+    printf("            ");
+    gotoxy(dino_x[4], dino_y[4] - y);
+    printf("                  ");
+    gotoxy(dino_x[5], dino_y[5] - y);
+    printf("                     ");
+    gotoxy(dino_x[6], dino_y[6] - y);
+    printf("                                ");
+    gotoxy(dino_x[7], dino_y[7] - y);
+    printf("                                ");
+    gotoxy(dino_x[8], dino_y[8] - y);
+    printf("                          ");
+    gotoxy(dino_x[9], dino_y[9] - y);
+    printf("                    ");
+    gotoxy(dino_x[10], dino_y[10] - y);
+    printf("                  ");
+    //legs
+    gotoxy(dino_x[11], dino_y[11] + y);
+    printf("              ");
+    gotoxy(dino_x[12], dino_y[12] + y);
+    printf("           ");
+    gotoxy(dino_x[13], dino_y[13] + y);
+    printf("          ");
+    gotoxy(dino_x[14], dino_y[14] + y);
+    printf("           ");
+
+    gotoxy(dino_x[0], dino_y[0]);
+    printf("%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[1], dino_y[1]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 254, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[2], dino_y[2]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[3], dino_y[3]);
+    printf("%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[4], dino_y[4]);
+    printf("%c%c%c%c%c%c%c%c%c",  221, 221, 221, 221, 221, 221, 221, 221,221);
+    gotoxy(dino_x[5], dino_y[5]);
+    printf("%c     %c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[6], dino_y[6]);
+    printf("%c%c    %c%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[7], dino_y[7]);
+    printf("%c%c%c  %c%c%c%c%c%c%c%c%c%c  %c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[8], dino_y[8]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[9], dino_y[9]);
+    printf("%c%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[10], dino_y[10]);
+    printf("%c%c%c%c%c%c%c%c%c", 221, 221, 221, 221, 221, 221, 221, 221, 221);
+
+    //legs
+    gotoxy(dino_x[11], dino_y[11]);
+    printf("%c%c%c  %c%c%c", 221, 221, 221, 221, 221, 221);
+    gotoxy(dino_x[12], dino_y[12]);
+    printf("%c%c   %c%c", 221, 221, 221, 221);
+    gotoxy(dino_x[13], dino_y[13]);
+    printf("%c%c    %c", 221, 221, 221);
+    gotoxy(dino_x[14], dino_y[14]);
+    printf("%c%c   %c%c", 221, 221, 221, 221);
+    //ground
+    gotoxy(17, 33);
+    printf("------------");
+
+}
+
+void animation_dino_legs()
+{
+    if(clock() - animation_time >= 250)
+    {
+        animation_time = clock();
+        gotoxy(dino_x[11], dino_y[11]);
+        printf("%c%c%c  %c%c%c", 221, 221, 221, 221, 221, 221);
+        gotoxy(dino_x[12], dino_y[12]);
+        printf("%c%c   %c%c", 221, 221, 221, 221);
+        gotoxy(dino_x[13], dino_y[13]);
+        printf("      %c", 221);
+        gotoxy(dino_x[14], dino_y[14]);
+        printf("     %c%c", 221, 221);
+
+    }
+    else
+    {
+
+        gotoxy(dino_x[11], dino_y[11]);
+        printf("%c%c%c  %c%c%c", 221, 221, 221, 221, 221, 221);
+        gotoxy(dino_x[12], dino_y[12]);
+        printf("%c%c   %c%c", 221, 221, 221, 221);
+        gotoxy(dino_x[13], dino_y[13]);
+        printf("%c%c    ", 221, 221);
+        gotoxy(dino_x[14], dino_y[14]);
+        printf("%c%c     ", 221, 221);
+    }
+}
+
+void ground()
+{
+    int i;
+    for(i = 0; i < 100; i++)
+    {
+        gotoxy(0+i, 33);
+        printf("-");
+    }
+
+}
+
+void setup()
+{
+    system("mode con: lines=40 cols=100");
+    system("color a");
+    system("cls");
+    set_cactus(a, 1);
+    set_cactus(b, 2);
+    draw_dino();
+    ground();
+    score = 0;
+}
 
 void jump(char on_ground)
 {
-    if(cactus_x == dino_x && cactus_y == dino_y)
+    if(on_ground == 1 && (cactus_xB[2] == 23 || cactus_xA[2] == 23))
     {
         system("cls");
         printf("Game Over!\n");
         system("pause");
         system("cls");
         setup();
-        cactus_x = 80;
-        score = 0;
+    }
+    else if(on_ground == 1)
+    {
+        animation_dino_legs();
+        cactus(time_cactus);
+        draw_time_score();
     }
 
-    else if(on_ground == 1)
+    else
     {
 
         int i;
-        for(i = 0; i < 3; i++)
+        for(i = 0; i < 15; i++)
         {
-            dino_y -= 1;
-            gotoxy(dino_x, dino_y);
-            printf("A");
-            gotoxy(dino_x, dino_y+1);
-            printf(" ");
-            gotoxy(0, 0);
-            printf("X: %d | Y: %d", dino_x, dino_y);
-            cactus(time_dino - time_cactus);
-            delay(time_dino);
+            draw_dino_up(1);
+            cactus(1);
+            draw_time_score();
+            delay(time_dino - 1);
         }
 
-        for(i = 0; i < 3; i++)
+        for(i = 0; i < 15; i++)
         {
-            dino_y += 1;
-            gotoxy(dino_x, dino_y);
-            printf("A");
-            gotoxy(dino_x, dino_y-1);
-            printf(" ");
-            gotoxy(0, 0);
-            printf("X: %d | Y: %d", dino_x, dino_y);
-            cactus(time_dino - time_cactus);
-            delay(time_dino);
+            draw_dino_down(1);;
+            cactus(1);
+            draw_time_score();
+            delay(time_dino - 1);
         }
     }
 }
@@ -162,21 +488,16 @@ int main()
     {
         while(!kbhit())
         {
-            jump(0);
-            cactus(time_cactus);
-            time_score();
+            jump(1);
         }
 
         key = getch();
 
         if(key == ' ')
         {
-            jump(1);
-            time_score();
+            jump(0);
         }
 
-        
     }
+    return 0;
 }
-
-//"É SÓ USAR A INTUIÇAO" - CIBORGUE JESUS, 2019
